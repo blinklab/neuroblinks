c71bcd099610c807d3a63d7ee748449ee782e2d5
blinklab <sheiney@sas.upenn.edu>
2016-07-06 12:30:41 -0500

    Temporarily delete all folders

--- shared/resetStimTrials.m
+++
@@ -1,7 +0,0 @@
-function resetStimTrials()
-
-trials=getappdata(0,'trials');
-
-trials.stimnum=0;
-
-setappdata(0,'trials',trials);
--- tdt/tdt_communication/TDTcheckmode.m
+++
@@ -1,7 +0,0 @@
-function mode=TDTcheckmode()
-
-TDT=getappdata(0,'tdt');
-% TTX=getappdata(0,'ttx');
-% metadata=getappdata(0,'metadata');
-
-mode=TDT.GetSysMode();
